260115c81dce65bd038ed6b7707d830f6c479ed7
Kristen Peck <kpeck71@gmail.com>
2017-09-11 01:28:13 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 def square_array(numbers)
   numbers = [1, 2, 3]
   new_numbers = []
-  
+
   numbers.each do |number|
-    squared_numbers = number ** 2
-    new_numbers << squared_numbers
+    new_numbers << number ** 2
   end
 end
