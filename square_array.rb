47ee8c54e55b2c0700df756afb65c58a80c3f484
Kristen Peck <kpeck71@gmail.com>
2017-09-11 01:36:58 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,8 @@
-#def square_array(numbers)
-#  new_numbers = []
-
-#numbers.each do |number|
-    #new_numbers << number ** 2
-    #end
-  #return new_numbers
-#end
-
 def square_array(numbers)
-  squared = []
-  squared_2 = squared.collect { |number| number ** 2}
-  squared_2
+  new_numbers = []
+
+numbers.each do |number|
+    new_numbers << number ** 2
+    end
+  return new_numbers
 end
